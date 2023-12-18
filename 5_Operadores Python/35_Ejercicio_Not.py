8579be2c7ccac86acbf4385b58612452d4f7b798
estebanLoza <estebanm.hloza@gmail.com>
2023-12-17 22:54:14 -0600

    Avance ejercicio 35 curso python

--- 5_Operadores Python/35_Ejercicio_Not.py
+++ 5_Operadores Python/35_Ejercicio_Not.py
@@ -1,1 +1,14 @@
 #sobre este ejercicio se hace la practica sobre la logica NOT
+# Ademas vamos a invertir la logica del ejercicio anterior donde vemos sobre si el padre puede ir o no ir al juego de su hijo
+
+
+vacaciones = False
+diaDescanso = False
+
+if not (vacaciones or diaDescanso): # se pone en parentesis ahora si, por que queremos que la respuesta se invierta
+    print('Tiene deberes por hacer')
+else: #en este caso es verdadera por que desde un inicio estamos invirtiendo la respuesta
+    print('Puede asistir al juego')
+
+
+
