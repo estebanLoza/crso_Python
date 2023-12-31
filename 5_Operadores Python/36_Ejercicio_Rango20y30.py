4c9e13a554564ab861063c3838150dce5919b6e4
estebanLoza <estebanm.hloza@gmail.com>
2023-12-30 20:33:39 -0600

    .

---
+++ 5_Operadores Python/36_Ejercicio_Rango20y30.py
@@ -0,0 +1,12 @@
+#En este ejericio se seguira utilizando sobre los operadores logicos
+#Tenemos que preguntar a la persona sobre su edad, si esta persona tiene entre 20 y 30, estara dentro del rango
+
+
+edad_Persona = int(input('Escribe tu edad: '))
+edad = 0
+
+veintes = edad >= 20 and edad < 30
+print(veintes)
+
+treintas = edad >= 30 and  edad < 40
+print(treintas)
