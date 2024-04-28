1b5a801e0925c63d7dbde6105cbd4d246c778a22
Esteban Hernandez <108112975+estebanLoza@users.noreply.github.com>
2024-04-28 11:28:44 -0600

    update

--- 9_Funciones en Python/71_funciones_pyhton.py
+++ 9_Funciones en Python/71_funciones_pyhton.py
@@ -1,11 +1,1 @@
-def suma():
-	num1 = int(input("Ingrese el primer número: "))
-	
-	num2 = int(input("Ingrese el segundo numero: "))
-	
-	sumar = num1 + num2
-	
-	print(f"La suma total es de {sumar}")
-	
-
-suma()
+print("Hola mundo")
