0c7e5245f1d31a0d1230281886e71decff1da649
estebanLoza <estebanm.hloza@gmail.com>
2025-05-01 02:10:29 -0600

    Se guarda un programa para el entendimiento de la lista

--- EJERCICIOS/pillow.py
+++ EJERCICIOS/pillow.py
@@ -1,9 +1,21 @@
-## Funcion lambda para poner en python
-## En forma de funcion y de manera común como siempre se hace una operacio
-import math
+lista = []
 
-print("La siguiente operación (3+2/2.5)^2")
 
-operacion = math.pow((3 + 2) / (2 * 5), 2)
 
-print(f"El resultado de la operacion es de {operacion}")
+p = int(input("Ingresa la cantidad de datos que quieres guardar: "))
+
+for i in range(p):
+    datosGuardar = input(f"Ingrea el {i + 1} dato: ")
+    lista.append(datosGuardar)
+
+print("\n\n")
+
+for numeroPrint in range(len(lista)):
+    print(f" '{lista[numeroPrint]}' es el {numeroPrint + 1} de la lista")
+
+
+
+
+
+
+
